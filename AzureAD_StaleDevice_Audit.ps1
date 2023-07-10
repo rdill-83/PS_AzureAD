0c1861a4900f07b2e0f4b4d0460615d7a85f8569
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-09 23:18:23 -0700

    Update AzureAD_StaleDevice_Audit.ps1

--- AzureAD_StaleDevice_Audit.ps1
+++ AzureAD_StaleDevice_Audit.ps1
@@ -1,4 +1,4 @@
 # PS AzureAD Device - Stale Device Audit
 
 # Retrieve Devices Not Logged in 60+ Days:
-Get-AzureADDevice -All $True | Where {$_.ApproximateLastLogonTimeStamp -le (Get-Date).AddDays(-60)} | Select -Property DisplayName,AccountEnabled,DeviceID,DeviceOSType,DeviceOSVersion,DeviceTrustType,ApproximateLastLogonTimeStamp | Out-File "C:\AzureAD_Devices_Removed_$(Get-Date -f yyyy-MM-dd).txt"
+Get-AzureADDevice -All $True | Where {$_.ApproximateLastLogonTimeStamp -le (Get-Date).AddDays(-60)} | Select -Property DisplayName,AccountEnabled,DeviceID,DeviceOSType,DeviceOSVersion,DeviceTrustType,ApproximateLastLogonTimeStamp | Export-CSV -NoTypeInformation -Path "C:\AzureAD_Devices_Removed_$(Get-Date -f yyyy-MM-dd).txt"
